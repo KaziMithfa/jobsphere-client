a318d364a836923cd4ed2ccc8e1ea2e4cbfdf662
KaziMithfa <kazimithfa99@gmail.com>
2025-08-02 11:08:39 +0200

    Banner part is completed

---
+++ src/components/Carousel.jsx
@@ -0,0 +1,49 @@
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+// import required modules
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import Slide from "./Slide";
+
+import bgImg1 from "../assets/images/banner1.jpg";
+import bgImg2 from "../assets/images/banner2.jpg";
+import bgImg3 from "../assets/images/banner3.jpg";
+
+export default function Carousel() {
+  return (
+    <div className="container mx-auto px-6 py-10">
+      <Swiper
+        spaceBetween={30}
+        centeredSlides={true}
+        loop={true}
+        autoplay={{
+          delay: 5000,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <Slide image={bgImg1}></Slide>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <Slide image={bgImg2}></Slide>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <Slide image={bgImg3}></Slide>
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
+}
