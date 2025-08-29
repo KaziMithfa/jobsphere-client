61816165992f7dac89b86d2a15b25135f0699e46
KaziMithfa <kazimithfa99@gmail.com>
2025-08-29 07:58:58 +0200

    pagination is completed

--- src/pages/AllPages.jsx
+++ src/pages/AllPages.jsx
@@ -2,18 +2,45 @@ import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../provider/AuthProvider";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { el } from "date-fns/locale";
 
 const AllPages = () => {
   const { user } = useContext(AuthContext);
   const [jobs, setJobs] = useState([]);
+  const [itemsPerPage, setItemsPerPage] = useState(2);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [count, setCount] = useState(0);
 
   useEffect(() => {
     getData();
-  }, []);
+  }, [currentPage, itemsPerPage]);
+
+  useEffect(() => {
+    getCount();
+  });
 
   const getData = async () => {
-    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/jobs`);
+    const { data } = await axios.get(
+      `${
+        import.meta.env.VITE_API_URL
+      }/all-jobs?page=${currentPage}&size=${itemsPerPage}`
+    );
     setJobs(data);
+  };
+
+  const getCount = async () => {
+    const { data } = await axios.get(
+      `${import.meta.env.VITE_API_URL}/jobs-count`
+    );
+
+    setCount(data.count);
+  };
+
+  const numberofPages = Math.ceil(count / itemsPerPage);
+  const pages = [...Array(numberofPages).keys()].map((element) => element + 1);
+
+  const handlePaginationButton = (value) => {
+    setCurrentPage(value);
   };
 
   return (
@@ -38,7 +65,7 @@ const AllPages = () => {
                       className="py-3.5 px-4 text-sm font-normal text-left rtl:text-right text-gray-500"
                     >
                       <div className="flex items-center gap-x-3">
-                        <span>Title</span>
+                        <span> Job Title</span>
                       </div>
                     </th>
 
@@ -46,7 +73,7 @@ const AllPages = () => {
                       scope="col"
                       className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
                     >
-                      <span>Deadline</span>
+                      <span>Job Posting Date</span>
                     </th>
 
                     <th
@@ -54,7 +81,7 @@ const AllPages = () => {
                       className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
                     >
                       <button className="flex items-center gap-x-2">
-                        <span>Price Range</span>
+                        <span>Application Deadline</span>
                       </button>
                     </th>
 
@@ -62,17 +89,11 @@ const AllPages = () => {
                       scope="col"
                       className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
                     >
-                      Category
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500"
-                    >
-                      Description
+                      Salary Range
                     </th>
 
                     <th className="px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500">
-                      Edit
+                      View Details
                     </th>
                   </tr>
                 </thead>
@@ -84,47 +105,18 @@ const AllPages = () => {
                       </td>
 
                       <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        {new Date(job.applicationDeadline).toLocaleDateString()}
+                        {new Date(job.jobPostingDate).toLocaleDateString()}
                       </td>
 
                       <td className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap">
-                        ${job.salaryRange}
+                        {new Date(job.applicationDeadline).toLocaleDateString()}
                       </td>
-                      <td className="px-4 py-4 text-sm whitespace-nowrap">
-                        <div className="flex items-center gap-x-2">
-                          <p
-                            className={`px-3 py-1 rounded-full 
-                           text-xs ${
-                             job.category === "On Site" &&
-                             "text-blue-500 bg-blue-100/60"
-                           }
 
-                           ${
-                             job.category === "Remote" &&
-                             "text-emerald-500 bg-emerald-100/60"
-                           }
-
-                            ${
-                              job.category === "Hybrid" &&
-                              "text-pink-500 bg-pink-100/60"
-                            }
-
-                             ${
-                               job.category === "Part-Time" &&
-                               "text-red-500 bg-red-100/60"
-                             }
-
-                           `}
-                          >
-                            {job.category}
-                          </p>
-                        </div>
-                      </td>
                       <td
                         title=""
                         className="px-4 py-4 text-sm text-gray-500  whitespace-nowrap"
                       >
-                        {job.description.substring(0, 18)}...
+                        {job.salaryRange}
                       </td>
                       <td className="px-4 py-4 text-sm whitespace-nowrap">
                         <div className="flex items-center gap-x-6">
@@ -156,6 +148,77 @@ const AllPages = () => {
             </div>
           </div>
         </div>
+      </div>
+
+      {/* Pagination section starts here... */}
+
+      <div className="flex justify-center mt-12">
+        {/* Previous Button */}
+        <button
+          disabled={currentPage === 1}
+          onClick={() => handlePaginationButton(currentPage - 1)}
+          className="px-4 py-2 mx-1 text-gray-700 disabled:text-gray-500 capitalize bg-gray-200 rounded-md disabled:cursor-not-allowed disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:bg-blue-500  hover:text-white"
+        >
+          <div className="flex items-center -mx-1">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 mx-1 rtl:-scale-x-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M7 16l-4-4m0 0l4-4m-4 4h18"
+              />
+            </svg>
+
+            <span className="mx-1">previous</span>
+          </div>
+        </button>
+
+        {/* Numbers */}
+
+        {pages.map((btnNum) => (
+          <button
+            onClick={() => handlePaginationButton(btnNum)}
+            key={btnNum}
+            className={`hidden ${
+              currentPage === btnNum ? "bg-blue-500 text-white" : ""
+            } px-4 py-2 mx-1 transition-colors duration-300 transform  rounded-md sm:inline hover:bg-blue-500  hover:text-white`}
+          >
+            {btnNum}
+          </button>
+        ))}
+
+        {/* Next Button */}
+
+        <button
+          disabled={currentPage === numberofPages}
+          onClick={() => handlePaginationButton(currentPage + 1)}
+          className="px-4 py-2 mx-1 text-gray-700 transition-colors duration-300 transform bg-gray-200 rounded-md hover:bg-blue-500 disabled:hover:bg-gray-200 disabled:hover:text-gray-500 hover:text-white disabled:cursor-not-allowed disabled:text-gray-500"
+        >
+          <div className="flex items-center -mx-1">
+            <span className="mx-1">Next</span>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-6 h-6 mx-1 rtl:-scale-x-100"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M17 8l4 4m0 0l-4 4m4-4H3"
+              />
+            </svg>
+          </div>
+        </button>
       </div>
     </section>
   );
